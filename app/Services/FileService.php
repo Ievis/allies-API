9808a837e6af1d97259b43d610cda58c85cc9212
Ievis <levchaba@gmail.com>
2023-07-10 01:15:26 +0600

    avatar-default.png add

--- app/Services/FileService.php
+++ app/Services/FileService.php
@@ -8,7 +8,7 @@ class FileService
     {
         if (is_string($file)) return $file;
         return empty($file)
-            ? url('images/image-default.png')
+            ? url('images/avatar-default.png')
             : url($file->store('images'));
     }
 }
