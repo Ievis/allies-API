2cd75c001e7de02b3ed55ccee33c3a95919680d4
Ievis <levchaba@gmail.com>
2023-07-10 00:29:37 +0600

    UserFactory fix

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,8 +22,8 @@ class UserFactory extends Factory
         return [
             'name' => fake()->word(),
             'surname' => fake()->word(),
-            'description' => fake()->paragraph(),
-            'role_id' => Role::where('name', 'teacher')->orWhere('name', 'main_teacher')->inRandomOrder(),
+            'description' => fake()->paragraph(2),
+            'role_id' => Role::where('name', 'teacher')->orWhere('name', 'main_teacher')->inRandomOrder()->first()->id,
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
@@ -38,7 +38,7 @@ class UserFactory extends Factory
      */
     public function unverified()
     {
-        return $this->state(fn(array $attributes) => [
+        return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
